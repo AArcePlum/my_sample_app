c68ef3cb31e5d3005030e7cdb846b06d72d214f8
Alan Arce <aarce@plum.com.ar>
2022-11-23 19:24:42 -0300

    add friendly forwarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,14 +46,15 @@ class UsersController < ApplicationController
     # Confirms a logged-in user.
     def logged_in_user
       unless logged_in?
-      flash[:danger] = "Please log in."
-      redirect_to login_url, status: :see_other
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url, status: :see_other
+      end
     end
-  end
 
-  # Confirms the correct user.
-  def correct_user
-    @user = User.find(params[:id])
-    redirect_to(root_url, status: :see_other) unless current_user?(@user)
-  end
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url, status: :see_other) unless current_user?(@user)
+    end
 end
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -18,16 +18,18 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_select 'div.field_with_errors'       # preguntar si esta bien. capitulo 10 pag 580
   end
 
-  test "successful edit" do
-    log_in_as(@user)
+  test "successful edit with friendly forwarding" do
     get edit_user_path(@user)
-    assert_template 'users/edit'
+    assert_equal session[:forwarding_url], edit_user_url(@user)               # VER SI ESTA BIEN. PAG 604
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
     name = "Foo Bar"
     email = "foo@bar.com"
     patch user_path(@user), params: { user: { name: name,
                                               email: email,
                                               password: "",
                                               password_confirmation: "" } }
+    assert_equal session[:forwarding_url], nil                                # VER SI ESTA BIEN. PAG 604
     assert_not flash.empty?
     assert_redirected_to @user
     @user.reload
