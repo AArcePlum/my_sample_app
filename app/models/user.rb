68181c27d1760e46de8936dbf7435dfeae70813b
Alan Arce <aarce@plum.com.ar>
2022-12-23 11:02:14 -0300

    Add eager loading

--- app/models/user.rb
+++ app/models/user.rb
@@ -85,7 +85,10 @@ class User < ApplicationRecord
 
   # Returns a user's status feed.
   def feed
-    Micropost.where("user_id IN (?) OR user_id = ?", following_ids, id)
+    part_of_feed = "relationships.follower_id = :id or microposts.user_id = :id"
+    Micropost.left_outer_joins(user: :followers)
+              .where(part_of_feed, { id: id }).distinct
+              .includes(:user, image_attachment: :blob)
   end
 
   # Follows a user.
